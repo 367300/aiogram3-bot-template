6b2e6e34cf59956bdb1c6b42ed728a0e9f3d193f
Вячеслав Литвиненко <l24122018@yandex.ru>
2025-07-25 00:49:23 +0300

    Добавление секретов

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ from aiogram.filters.command import Command
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 from aiogram import F
 
+from decouple import config
+
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 
 # Замените "YOUR_BOT_TOKEN" на токен, который вы получили от BotFather
-API_TOKEN = 'YOUR_BOT_TOKEN'
+API_TOKEN = config('API_TOKEN_TG', default=False)
+
+if (not API_TOKEN):
+    logging.error("Необходимо указать токен для бота!")
+    exit()
 
 # Объект бота
 bot = Bot(token=API_TOKEN)
